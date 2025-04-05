dd974793ce45e8af2a41c54e2a6780b796c8083e
Arhariel <arharie@yandex.ru>
2025-04-05 17:24:16 +0500

    delete styles folders

--- components/404/404.js
+++ components/404/404.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 
 
 const Error = (props) => {
@@ -12,15 +11,12 @@ const Error = (props) => {
         <section className="error-404-section section-padding">
             <div className="container">
                 <div className="row">
-                    <div className="col col-xs-12">
+                    <div className="col col-xs-12 h-screen">
                         <div className="content clearfix">
-                            <div className="error">
-                                <Image src={erimg} alt=""/>
-                            </div>
                             <div className="error-message">
                                 <h3>Oops! Page Not Found!</h3>
                                 <p>We’re sorry but we can’t seem to find the page you requested. This might be because you have typed the web address incorrectly.</p>
-                                <Link onClick={ClickHandler} href="/pages/home-n" className="theme-btn"> Back to home</Link>
+                                <Link onClick={ClickHandler} href="/" className="theme-btn"> Back to home</Link>
                             </div>
                         </div>
                     </div>
