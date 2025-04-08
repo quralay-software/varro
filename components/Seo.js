ddee5f4c030518b0d2e7b408e7862957569728f2
Arhariel <arharie@yandex.ru>
2025-04-08 19:14:30 +0500

    schema seo

--- components/Seo.js
+++ components/Seo.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
+import { seoSchemas } from "./seoSchemas";
 
-const Seo = ({ title, description, image = "/images/social-share.jpg" }) => {
+const Seo = ({ title, description, image = "/images/img6.webp" }) => {
   const { locale, asPath } = useRouter();
   const { t } = useTranslation("common");
 
@@ -12,17 +13,15 @@ const Seo = ({ title, description, image = "/images/social-share.jpg" }) => {
   const pageDescription = description || t(`${slug}.description`);
   const url = `https://varro.kz${asPath}`;
 
+  const autoSchema = seoSchemas[slug] || seoSchemas["main"];
+
   return (
     <Head>
       <title>{pageTitle}</title>
       <meta name="description" content={pageDescription} />
 
       <link rel="canonical" href={url} />
-      <link
-        rel="alternate"
-        hrefLang="ru"
-        href={`https://varro.kz/ru${asPath}`}
-      />
+
       <link
         rel="alternate"
         hrefLang="kk"
@@ -44,6 +43,13 @@ const Seo = ({ title, description, image = "/images/social-share.jpg" }) => {
       <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:description" content={pageDescription} />
       <meta name="twitter:image" content={image} />
+
+      {autoSchema && (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(autoSchema) }}
+        />
+      )}
     </Head>
   );
 };
